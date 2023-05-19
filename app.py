ed724772194682d33f209b159d2b76ebe00b6a10
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-05-19 18:35:01 -0500

    added todo

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
 # Global variable for storing state
-state = {'query': 0, 'result':None}
+
+# query has values in [0,3] with the following mapping:
+# 0 - no query yet
+# 1 - fetchFromTable
+# 2 - fetchChangeData
+# 3 - fetchAggregateData
+
+# result maintains the processed results from the last query
+
+state = {'query': 0, 'result':None, 'queryString':""}
+
+# TODO:
+# 1. Making API calls - from where, building query, storing results
+# 2. Adding CSV data (API post call)
 
 @app.route('/')
 def index():
+    if state["query"]: #fetch query and update result
+        #front end updates query string and query int and triggers a page reload? 
+        state["result"] = None #perform call and get result
+
     return render_template('index.html', initial_state=state)
 
 if __name__ == '__main__':
